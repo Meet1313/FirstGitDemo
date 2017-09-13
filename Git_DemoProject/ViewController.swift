3cce259a1b280b758e5c693af6c5ce7bfd0eaea4
PanktiPC <pankti@PanktiPCs-iMac.local>
2017-09-13 18:36:06 +0530

    Initial Commit

---
+++ Git_DemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git_DemoProject
+//
+//  Created by PanktiPC on 13/09/17.
+//  Copyright © 2017 PanktiPC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
